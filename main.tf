7e67010b5dfe21a81b5f23d33d6d13319a189038
faidhoi <faidhoiyouusouf@gmail.com>
2026-04-20 16:19:19 +0000

    Ajout de l'infrastructure Terraform (simulation)

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.0"
+    }
+  }
+}
+
+resource "random_string" "vpc_id" {
+  length  = 8
+  special = false
+  upper   = false
+}
+
+resource "random_string" "instance_ip" {
+  length  = 12
+  special = false
+  upper   = false
+}
+
+output "vpc_id" {
+  value = "vpc-${random_string.vpc_id.result}"
+}
+
+output "instance_ip" {
+  value = "192.168.${random_string.instance_ip.result}"
+}
